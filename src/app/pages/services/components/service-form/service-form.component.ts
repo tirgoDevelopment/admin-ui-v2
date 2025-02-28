a6498a09079c759d31d7c4eb6e5a845939339b20
Najmiddin <najmiddinshonasriddinov@gmail.com>
2025-02-28 17:22:13 +0500

    last

--- src/app/pages/services/components/service-form/service-form.component.ts
+++ src/app/pages/services/components/service-form/service-form.component.ts
@@ -1,59 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { debounceTime, distinctUntilChanged, switchMap, catchError, of, BehaviorSubject, Observable } from 'rxjs';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { DriversService } from 'src/app/pages/drivers/services/drivers.service';
 import { CommonModules } from 'src/app/shared/modules/common.module';
 import { NzModules } from 'src/app/shared/modules/nz-modules.module';
 import { PipeModule } from 'src/app/shared/pipes/pipes.module';
-import { generateQueryFilter } from 'src/app/shared/pipes/queryFIlter';
 import { ServicesService } from '../../services/services.service';
 import { ServiceModel } from '../../models/service.model';
-import { Response } from 'src/app/shared/models/reponse';
-import { NzDrawerRef } from 'ng-zorro-antd/drawer';
+import { NzDrawerRef, NzDrawerService } from 'ng-zorro-antd/drawer';
 import { Permission } from 'src/app/shared/enum/per.enum';
 import { PermissionService } from 'src/app/shared/services/permission.service';
 import { LabelPipe } from 'src/app/shared/pipes/label.pipe';
+import { PriceFormatPipe } from 'src/app/shared/pipes/priceFormat.pipe';
+import { PhoneFormatPipe } from 'src/app/shared/pipes/phone-format.pipe';
+import { DetailComponent } from 'src/app/pages/merchant/merchant-driver/components/detail/detail.component';
 
 @Component({
   selector: 'app-service-form',
   templateUrl: './service-form.component.html',
   styleUrls: ['./service-form.component.scss'],
   standalone: true,
-  imports: [NzModules, TranslateModule, CommonModules, PipeModule, LabelPipe],
+  imports: [NzModules, TranslateModule, CommonModules, PipeModule, LabelPipe,PriceFormatPipe,PhoneFormatPipe],
 })
 export class ServiceFormComponent implements OnInit {
   Per = Permission;
   form: FormGroup;
   services: ServiceModel[] = [];
-  searchDriver$ = new BehaviorSubject<string>('');
-  drivers$: Observable<any>;
+  selectedDriver
+  drivers = []
   loading: boolean = false;
   constructor(
     private driverService: DriversService,
     private serviceApi: ServicesService,
     private drawerRef: NzDrawerRef,
-    public perService: PermissionService
+    public perService: PermissionService,
+    private translate: TranslateService,
+    private  drawer: NzDrawerService
   ) { }
   ngOnInit(): void {
     this.initForm();
-    this.drivers$ = this.searchDriver$.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((searchTerm: string) => this.driverService.getAll({}, searchTerm).pipe(
-        catchError((err) => {
-          return of({ data: { content: [] } });
-        })
-      )),
-    );
     this.getServices();
+    this.onDriverChange();
   }
 
   findDriver(ev: string) {
-    let searchAs = this.form.get('searchAs')?.value;
-    let filter = generateQueryFilter({ [searchAs]: ev });
-    this.searchDriver$.next(filter);
+    if (ev) {
+      this.driverService.findDrivers(ev, this.form.value.searchAs).subscribe((res: any) => {
+        if (res) {
+          this.drivers = res.data.content;
+        }
+      })
+    }
   }
+
   getServices() {
     this.serviceApi.getServiceList().subscribe((res: any) => {
       if (res.data && Array.isArray(res.data)) {
@@ -92,5 +91,25 @@ export class ServiceFormComponent implements OnInit {
     }, err => {
       this.loading = false;
     });
+  }
+  onDriverChange() {
+    this.form.get('driverId')?.valueChanges.subscribe(selectedValue => {
+      this.selectedDriver = this.drivers.find(driver => driver.id == selectedValue);
+    });
+  }
+  showTms(id) {
+    if (id && this.perService.hasPermission(this.Per.TmsDetail)) {
+      const drawerRef: any = this.drawer.create({
+        nzTitle: this.translate.instant('information'),
+        nzContent: DetailComponent,
+        nzMaskClosable: false,
+        nzPlacement: 'right',
+        nzWidth: '400px',
+        nzContentParams: {
+          id: id,
+        }
+      });
+    }
+
   }
 }
